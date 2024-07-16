c0489d351bc0a254a4eff6d7d6bf418aa265ff6d
Martin Tracey <matracey@outlook.ie>
2024-07-16 03:14:58 -0700

    Create TodoService

---
+++ src/TodoBlazorApp/Services/TodoService.cs
@@ -0,0 +1,39 @@
+using System.Net.Http.Json;
+using TodoBlazorApp.Models;
+
+namespace TodoBlazorApp.Services;
+
+public class TodoService
+{
+    private readonly HttpClient _httpClient;
+
+    public TodoService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<TodoItem>> GetTodosAsync()
+    {
+        return await _httpClient.GetFromJsonAsync<IEnumerable<TodoItem>>("api/todo") ?? [];
+    }
+
+    public async Task<TodoItem?> GetTodoAsync(long id)
+    {
+        return await _httpClient.GetFromJsonAsync<TodoItem>($"api/todo/{id}");
+    }
+
+    public async Task AddTodoAsync(TodoItem todo)
+    {
+        await _httpClient.PostAsJsonAsync("api/todo", todo);
+    }
+
+    public async Task UpdateTodoAsync(TodoItem todo)
+    {
+        await _httpClient.PutAsJsonAsync($"api/todo/{todo.Id}", todo);
+    }
+
+    public async Task DeleteTodoAsync(long id)
+    {
+        await _httpClient.DeleteAsync($"api/todo/{id}");
+    }
+}
