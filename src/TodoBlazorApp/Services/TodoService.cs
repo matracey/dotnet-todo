a0c66be2234a950a0853e85d3aaaa79b595d20bd
Martin Tracey <matracey@outlook.ie>
2024-07-16 03:15:31 -0700

    Add `BaseAddress` configuration to `HttpClient`.

--- src/TodoBlazorApp/Services/TodoService.cs
+++ src/TodoBlazorApp/Services/TodoService.cs
@@ -9,7 +9,8 @@ public class TodoService
 
     public TodoService(HttpClient httpClient)
     {
-        _httpClient = httpClient;
+          _httpClient = httpClient;
+          _httpClient.BaseAddress = new Uri("https://localhost:5000/"); // Update the port number if necessary
     }
 
     public async Task<IEnumerable<TodoItem>> GetTodosAsync()
